a2d22e86dc0cac180120d08eaeb93b04b53770ec
ZiyaSenior <safaraliyevziya@gmail.com>
2026-03-15 15:36:58 +0000

    tasks

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Welcome to"
 str2 = "Holberton School!"
-# print the concatenated string using str1 and str2
+# Concatenate str1 and str2 and print the result
 print(f"{str1} {str2}")
