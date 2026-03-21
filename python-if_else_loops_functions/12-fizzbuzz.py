2dbb1b8b570b8634596bf543ae584081951a4af6
ZiyaSenior <safaraliyevziya@gmail.com>
2026-03-21 22:52:32 +0000

    tasks

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# function that prints numbers from 1 to 100 with FizzBuzz rules
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print("{}".format(i), end=" ")
