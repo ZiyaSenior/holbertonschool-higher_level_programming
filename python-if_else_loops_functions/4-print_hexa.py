7faa1261a7afd1171084e27f0752fc88403610d6
ZiyaSenior <safaraliyevziya@gmail.com>
2026-03-21 22:31:06 +0000

    tasks

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
+for i in range(97, 123):
+    if i not in (101, 113):
+        print("{}".format(chr(i)), end="")
 
-# print numbers from 0 to 98 in decimal and hexadecimal with 0x prefix
-for i in range(99):
-    print("{} = 0x{:x}".format(i, i)) 
