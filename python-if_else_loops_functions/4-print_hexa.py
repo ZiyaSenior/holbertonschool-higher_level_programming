6472b32af4138316546792b37d77bef291d141e7
ZiyaSenior <safaraliyevziya@gmail.com>
2026-03-21 22:26:23 +0000

    tasks

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-# print numbers from 0 to 98 in decimal and hexadecimal
+# print numbers from 0 to 98 in decimal and hexadecimal with one print
 for i in range(99):
-    if i != 98:
-        print("{:d} = {:x}, ".format(i, i), end="")
-    else:
-        print("{:d} = {:x}".format(i, i), end="") 
+    print("{:d} = {:x}".format(i, i), end=", " if i != 98 else "") 
