977dcf814216780b36440588a7295658c54cd8d2
ZiyaSenior <safaraliyevziya@gmail.com>
2026-03-22 00:57:51 +0000

    tasks

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-
-# import functions from calculator_1.py only once
 from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
     a = 10
     b = 5
-    results = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
-    operations = ["+", "-", "*", "/"]
-
-    for op, res in zip(operations, results):
-        print("{} {} {} = {}".format(a, op, b, res))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
