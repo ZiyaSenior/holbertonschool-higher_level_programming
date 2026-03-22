117144e090847910ec932bd8229b36803f371421
ZiyaSenior <safaraliyevziya@gmail.com>
2026-03-22 01:34:20 +0000

    tasks

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    new_list = []
-    for i in my_list:
-        if i % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
 
-    return new_list  
+# function that returns a list of True/False for multiples of 2
+def divisible_by_2(my_list=[]):
+    result = []
+    for num in my_list:
+        result.append(num % 2 == 0)
+    return result  
